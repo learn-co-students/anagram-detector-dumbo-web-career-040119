dd2b22746457f124f2be86eb6c570e4e06a9e8a6
Matthew Bartl <mattbartl@gmail.com>
2019-04-08 13:31:10 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word, :word_array
+  
+  def initialize(word)
+    @word = word.reverse
+    @word_array = word.split("").sort
+  end
+  
+  def match(array)
+    array.select do |item|
+      item.split("").sort == word_array
+    end
+  end
+  
+end
